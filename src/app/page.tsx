1e283179c3c0fdfc8fec4be3b8007624cf58b5bc
rishinhk004 <rishirajsaha555@gmail.com>
2024-12-30 20:26:51 +0530

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+const HomePage = () =>{
+  return (
+   <main className="container mx-auto bg-white">
+    <h1 className="text-black">Home Page</h1>
+    </main>
+  );
+}
+
+export default HomePage;
