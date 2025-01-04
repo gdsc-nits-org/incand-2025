9c2747a320ff2cca3d83e5bfbddd8d5bafb6d9c1
byt3h3ad <adhirajd007@gmail.com>
2025-01-04 11:27:51 +0530

    style: linting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-const HomePage = () =>{
+const HomePage = () => {
   return (
-   <main className="container mx-auto bg-white">
-    <h1 className="text-black">Home Page</h1>
+    <main className="container mx-auto bg-white">
+      <h1 className="text-black">Home Page</h1>
     </main>
   );
-}
+};
 
 export default HomePage;
