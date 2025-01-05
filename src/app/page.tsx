129900fd31d43ce63eca04afec2bce449d350355
rishinhk004 <rishirajsaha555@gmail.com>
2025-01-05 12:27:26 +0530

    build fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
-
+import HiddenQuest from "~/components/HiddenQuest/HiddenQuest";
+import Button from "~/components/FooterButton/Button";
+export const runtime = "edge";
 const HomePage = () => {
   return (
     <main className="container mx-auto bg-white">
       <h1 className="text-black">Home Page</h1>
+      <HiddenQuest/>
+      <Button/>
     </main>
   );
 };
--- src/components/FooterButton/Button.tsx
+++ src/components/FooterButton/Button.tsx
@@ -1,30 +1,54 @@
-
 import styles from "./Button.module.css";
+
 const Button = () => {
   return (
     <div className={styles.container}>
-    <a href="#" className={styles.button}>
-       <div className={styles['button__content']}>
-       <span className={`${styles['button__text']} font-neue-haas-grotesk`}>
-        BUTTON
-       </span>
-
-
-          <div className={styles['button__reflection_1']}></div>
-          <div className={styles['button__reflection_2']}></div>
-       </div>
-
-       <img src="/star1.png" alt="" className={`${styles['button__star_1']} ${styles.image}`}/>
-       <img src="/star1.png" alt="" className={`${styles['button__star_2']} ${styles.image}`}/>
-       <img src="/star1.png" alt="" className={`${styles['button__star_3']} ${styles.image}`}/>
-       <img src="/star1.png" alt="" className={`${styles['button__star_4']} ${styles.image}`}/>
-       <img src="/star1.png" alt="" className={`${styles['button__star_5']} ${styles.image}`}/>
-       <img src="/star1.png" alt="" className={`${styles['button__star_6']} ${styles.image}`}/>
-       <img src="/star1.png" alt="" className={`${styles['button__star_7']} ${styles.image}`}/>
-      
-       <div className={styles['button__shadow']}></div>
-    </a>
- </div>
+      <a href="#" className={styles.button}>
+        <div className={styles.button__content}>
+          <span className={`${styles.button__text} font-neue-haas-grotesk`}>
+            BUTTON
+          </span>
+          <div className={styles.button__reflection_1}></div>
+          <div className={styles.button__reflection_2}></div>
+        </div>
+        <img
+          src="/star1.png"
+          alt=""
+          className={`${styles.button__star_1} ${styles.image}`}
+        />
+        <img
+          src="/star1.png"
+          alt=""
+          className={`${styles.button__star_2} ${styles.image}`}
+        />
+        <img
+          src="/star1.png"
+          alt=""
+          className={`${styles.button__star_3} ${styles.image}`}
+        />
+        <img
+          src="/star1.png"
+          alt=""
+          className={`${styles.button__star_4} ${styles.image}`}
+        />
+        <img
+          src="/star1.png"
+          alt=""
+          className={`${styles.button__star_5} ${styles.image}`}
+        />
+        <img
+          src="/star1.png"
+          alt=""
+          className={`${styles.button__star_6} ${styles.image}`}
+        />
+        <img
+          src="/star1.png"
+          alt=""
+          className={`${styles.button__star_7} ${styles.image}`}
+        />
+        <div className={styles.button__shadow}></div>
+      </a>
+    </div>
   );
 };
 
