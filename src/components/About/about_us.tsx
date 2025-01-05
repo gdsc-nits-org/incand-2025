bc8cccc6b67d257e9a5d8eb12ca5f1b3c60fcc4d
Newt-Shadow <anmol.s.sahoo@gmail.com>
2025-01-05 10:44:32 +0530

    check responsiveness

--- src/components/About/about_us.tsx
+++ src/components/About/about_us.tsx
@@ -4,7 +4,7 @@ import styles from './about.module.css';
 const AboutUs = () => {
     return (
         <div className="flex items-center relative h-screen lg:w-screen md:w-screen w-screen justify-center">
-            <div className="inset-0 flex flex-col items-center bg-[url('/assets/images/maze.png')] bg-cover bg-center bg-no-repeat absolute bg-[#FFA6F6]">
+            <div className="inset-0 flex items-center bg-[url('/assets/images/maze.png')] bg-cover bg-no-repeat absolute bg-[#FFA6F6]">
                 <div className={`absolute w-[66.62%] h-[63%] justify-center sm:rotate-[-4.21deg] lg:rotate-[-4.21deg] rounded-[36px] border-black border-4 bg-[#FBFAF0] ${styles.box1}`}>
                     <div className="absolute flex w-[100%] bg-[#FFF59F] h-[11.23%] border-b-black border-b-2 rounded-tl-[36px] rounded-tr-[36px] items-center">
                         <div className="w-[2.07%] h-[34.7%] bg-[#FFA6F6] rounded-[50%] ml-3 mx-1 rotate-[-4.21deg] px-3 lg:ml-5 md:px-3 lg:mx-1 lg:px-3 "></div>
@@ -22,17 +22,17 @@ const AboutUs = () => {
                         <Image
                             src="/assets/images/about.png"
                             alt="About Us Image"
-                            className="  rotate-[4.21deg] h-[40.44%] w-[84%]  sm:h-[15.44%] sm:w-[70%] md:h-[20.44%] md:w-[75%] sm:rotate-[4.21deg] lg:h-[19.44%] lg:w-[75%] lg:rotate-[4.21deg] xl:h-[24.44%] xl:w-[45%]"
+                            className="  rotate-[4.21deg] h-[40.44%] w-[84%]  sm:h-[15.44%] sm:w-[70%] md:h-[20.44%] md:w-[75%] sm:rotate-[4.21deg] lg:h-[19.44%] lg:w-[28%] lg:rotate-[4.21deg] xl:h-[24.44%] xl:w-[45%]  "
                             width={600} 
                             height={400}
                         />
                     </div>
-                    <div className="relative top-[17%] flex justify-center align-middle xl:top-[12%] ">
-                        <p className={`font-tusker tracking-normal text-[25px] w-[100%] leading-[2.4rem] sm:leading-[3.2rem] sm:text-[34px]  md:leading-[3.3rem] md:text-[38px] lg:text-[40px] lg:leading-[3.6rem] xl:leading-[3.5rem] text-center ${styles.smallFont} `}>
+                    <div className="relative top-[17%] flex justify-center align-middle xl:top-[12%] md:top-[14%] ">
+                        <p className={`font-tusker tracking-normal text-[25px] w-[100%] leading-[2.4rem] sm:leading-[3.2rem] sm:text-[34px]  md:leading-[3.3rem] md:text-[34px] lg:text-[40px] lg:leading-[3.6rem] xl:leading-[4.8rem] xl:tracking-wide text-center ${styles.smallFont} `}>
                             NIT Silchar&apos;s cultural extravaganza, 
                             <svg
                                 xmlns="http://www.w3.org/2000/svg"
-                                className="inline-block w-[80%] sm:w-[80%] sm:h-[50] lg:mt-2 "
+                                className="inline-block w-[80%] sm:w-[80%] sm:h-[50] lg:mt-2 xl:w-[25%]"
                                 height="37"
                                 viewBox="0 0 300 100"
                             >
