f2eaa86eab62116e710cde6562c7145bcff0b468
rishinhk004 <rishirajsaha555@gmail.com>
2025-01-05 12:28:38 +0530

    conflict

--- src/components/HiddenQuest/HiddenQuest.tsx
+++ src/components/HiddenQuest/HiddenQuest.tsx
@@ -1,10 +1,8 @@
-
 "use client"; // Add this at the top to mark the file as a Client Component
 
 import { useState } from "react";
-const HiddenQuest = () =>{
-
-    const [isVisible, setIsVisible] = useState(false); // State for visibility
+const HiddenQuest = () => {
+  const [isVisible, setIsVisible] = useState(false); // State for visibility
 
   const handleOpen = () => {
     setIsVisible(true); // Show HiddenQuest component
@@ -17,83 +15,134 @@ const HiddenQuest = () =>{
     }
   };
 
-      return (
-        <div className="w-full h-[50vh] overflow-y-auto shadow-gray-600">
+  return (
+    <div className="h-[50vh] w-full overflow-y-auto shadow-gray-600">
+      <button
+        className="top-6rounded-md fixed left-[50%] m-auto"
+        onClick={handleOpen}
+      >
+        <img src="/Button1.png" className="h-[15%] w-[15%]" />
+      </button>
+
+      {isVisible && (
+        <div className="box-border h-[100vh] w-full bg-black">
+          <div className="relative box-border h-[50vh] w-full overflow-hidden bg-gray-100 bg-[url('/bgImage.gif')] bg-[length:100%_100%]">
+            <div className="absolute right-0 top-0 box-border h-[20%] w-[8%] bg-blue-600">
+              <img
+                src="/cross.gif"
+                className="h-[100%] w-[100%]"
+                onClick={() => setIsVisible(false)}
+              ></img>
+            </div>
+
+            <div className="absolute left-[30%] top-[18%] box-border flex h-[20vh] w-[50vw] flex-col items-center justify-center gap-3">
+              <h1 className="decoration-skip-ink-none text-left font-ahsing text-[500%] font-[400] text-white">
+                Hidden Quest
+              </h1>
+              <p className="text-left font-ahsing text-[38.22px] font-normal leading-[48.22px] text-white">
+                find <span className="text-yellow-500">INCAND</span>ESCENCE
+              </p>
+            </div>
+            <div className="absolute bottom-0 right-0 box-border flex h-[30%] w-[18%] flex-col items-center justify-center text-left">
+              <p className="text-left font-ahsing text-[22.22px] font-normal leading-tight text-white">
+                Scroll to
+              </p>
+              <p className="text-left font-ahsing text-[22.22px] font-normal leading-tight text-white">
+                know more
+              </p>
+            </div>
             <button
-          className="fixed m-auto left-[50%] top-6rounded-md "
-         onClick={handleOpen}
-           >
-          <img src="/Button1.png" className="w-[15%] h-[15%]"/>
-         </button>
+              className="absolute bottom-0 left-[40%] h-[30%] w-[30%]"
+              onClick={handleScrollToNext}
+            >
+              <div className="group relative flex h-[100%] w-[100%] flex-row justify-between">
+                <img
+                  src="/mainStar.png"
+                  className="absolute bottom-7 left-[40%] inline-block h-[70%] w-[30%] transition-all duration-300 group-hover:bottom-0"
+                />
+                <img
+                  src="/mainStar.png"
+                  className="absolute -bottom-7 left-0 h-[70%] w-[30%] transition-all duration-300 group-hover:bottom-0"
+                />
+                <img
+                  src="/mainStar.png"
+                  className="absolute -bottom-7 right-0 h-[70%] w-[30%] transition-all duration-300 group-hover:bottom-0"
+                />
+              </div>
+            </button>
+          </div>
 
-         {isVisible &&( <div className="w-full box-border h-[100vh] bg-black ">
-             <div className="w-full box-border  h-[50vh] relative overflow-hidden bg-gray-100 bg-[url('/bgImage.gif')] bg-[length:100%_100%]">
-                <div className="absolute box-border top-0 right-0 bg-blue-600 w-[8%] h-[20%]">
-                    <img src="/cross.gif" className="w-[100%] h-[100%] " onClick={() => setIsVisible(false)} ></img>
-                </div>
+          <div
+            id="secondDiv"
+            className="relative box-border h-[50vh] w-full overflow-hidden"
+          >
+            <img
+              src="/image1.png"
+              className="absolute -left-[10%] -top-[40%] h-[full] w-[40%] rotate-[220.26deg]"
+            />
 
-                <div className="top-[18%] gap-3 left-[30%] box-border flex flex-col justify-center items-center absolute w-[50vw] h-[20vh]">
-                    <h1 className="font-ahsing text-[500%] font-[400] text-white text-left decoration-skip-ink-none"
-                    >Hidden Quest</h1>
-                    <p className="font-ahsing text-[38.22px] font-normal text-white leading-[48.22px] text-left ">find <span className="text-yellow-500">INCAND</span>ESCENCE</p>
-                </div>
-                <div className=" absolute box-border flex flex-col justify-center text-left items-center h-[30%] w-[18%] right-0 bottom-0">
-                    <p className="font-ahsing leading-tight text-[22.22px] font-normal text-white text-left ">Scroll to</p>
-                    <p className="font-ahsing leading-tight text-[22.22px] font-normal  text-white text-left ">know more</p>
-                </div>
-                <button className="absolute w-[30%] h-[30%] bottom-0 left-[40%] " onClick={handleScrollToNext}>
-                                    <div className="w-[100%] h-[100%] relative flex flex-row justify-between group">
-                    <img  src="/mainStar.png"  className="w-[30%] h-[70%] inline-block absolute left-[40%] bottom-7 group-hover:bottom-0 transition-all duration-300"/>  
-                    <img  src="/mainStar.png"  className="w-[30%] h-[70%] absolute left-0 -bottom-7 group-hover:bottom-0 transition-all duration-300"/> 
-                    <img  src="/mainStar.png"  className="w-[30%] h-[70%] absolute right-0 -bottom-7 group-hover:bottom-0 transition-all duration-300"/>
-               </div>                   
-                </button>
-           </div>
-            
-            
-            <div id="secondDiv" className="relative box-border w-full h-[50vh] overflow-hidden " >
-               
-               <img src="/image1.png" className="w-[40%] h-[full] absolute -left-[10%] -top-[40%] rotate-[220.26deg] "/>
-                
+            <img
+              src="/image1.png"
+              className="absolute -right-[10%] -top-[45%] h-[full] w-[40%] -rotate-[220.26deg]"
+            />
+            <img
+              src="/Gif.gif"
+              className="absolute -top-[7%] left-[35%] h-[full] w-[30%]"
+            />
 
-               <img src="/image1.png" className="w-[40%] h-[full] absolute -right-[10%] -top-[45%] -rotate-[220.26deg] "/>
-               <img src="/Gif.gif" className="w-[30%] h-[full] absolute left-[35%]  -top-[7%]"/>
+            <div className="absolute right-0 top-0 box-border h-[20%] w-[8%] bg-blue-600">
+              <img
+                src="/cross.gif"
+                className="h-[100%] w-[100%]"
+                onClick={() => setIsVisible(false)}
+              ></img>
+            </div>
+            <div className="absolute left-[40%] top-[15%] h-[10vh] w-[40vw]">
+              <h1 className="text-left font-ahsing text-[54px] font-normal leading-[64px] text-white">
+                How it works ?
+              </h1>
+            </div>
+            <div className="absolute bottom-0 left-[8%] h-[30vh] w-[20vw] gap-3">
+              <div className="box-border h-[22%] w-[40%] rounded-sm border-2 border-black bg-yellow-400 p-2 font-bricolage text-[24.06px] font-extrabold leading-[28.87px] shadow-custom-white">
+                Step 01
+              </div>
+              <p className="mt-4 text-left font-bricolage text-[14px] leading-tight tracking-wider text-[#D3D3D3]">
+                Register in various <br />
+                events to get a letter <br />
+                from the word
+              </p>
+              <img
+                src="/oneStar.png"
+                className="ml-[20%] mt-[5%] inline-block h-[15%] w-[15%] rotate-180 invert filter"
+              />
+              <img src="/incand.png" className="-mt-4 block h-[20%] w-[60%]" />
+            </div>
+            <div className="absolute -bottom-[8%] left-[40%] h-[25vh] w-[20vw]">
+              <div className="mx-auto box-border h-[22%] w-[40%] rounded-sm border-2 border-black bg-yellow-400 p-2 font-bricolage text-[24.06px] font-extrabold leading-[28.87px] shadow-custom-white">
+                Step 02
+              </div>
 
-               <div className="absolute box-border top-0 right-0 bg-blue-600 w-[8%] h-[20%]">
-                    <img src="/cross.gif" className="w-[100%] h-[100%] " onClick={() => setIsVisible(false)} ></img>
-                </div>
-                <div className="w-[40vw] h-[10vh] absolute left-[40%] top-[15%] ">
-                    <h1 className="font-ahsing text-[54px] text-white font-normal leading-[64px] text-left ">How it works ?</h1>
-                </div>
-                <div className="w-[20vw] h-[30vh] gap-3 absolute left-[8%] bottom-0 ">
-                    <div className="w-[40%] h-[22%] box-border border-black border-2 rounded-sm shadow-custom-white bg-yellow-400 font-bricolage text-[24.06px] font-extrabold leading-[28.87px] p-2">Step 01</div>
-                    <p className="font-bricolage tracking-wider leading-tight text-[14px] mt-4 text-[#D3D3D3]  text-left ">Register in various <br/>
-                    events to get a letter <br/>
-                    from the word</p>
-                    <img src="/oneStar.png" className="w-[15%] h-[15%] filter invert rotate-180 inline-block ml-[20%] mt-[5%]"/>
-                    <img src="/incand.png" className="w-[60%] h-[20%] -mt-4 block "/>
-                </div>
-                <div className="w-[20vw] h-[25vh] absolute left-[40%]  -bottom-[8%] ">
-                <div className="w-[40%] h-[22%] mx-auto box-border border-black border-2 rounded-sm shadow-custom-white bg-yellow-400 font-bricolage text-[24.06px] font-extrabold leading-[28.87px] p-2">Step 02</div>
+              <p className="mt-4 text-center font-bricolage text-[14px] leading-tight tracking-wider text-[#D3D3D3]">
+                Share with your friends or <br />
+                request them to collect all six <br />
+                letters of the fest!
+              </p>
+            </div>
+            <div className="absolute bottom-0 right-[8%] h-[30vh] w-[20vw]">
+              <div className="mx-[57%] box-border h-[22%] w-[40%] rounded-sm border-2 border-black bg-yellow-400 p-2 font-bricolage text-[24.06px] font-extrabold leading-[28.87px] shadow-custom-white">
+                Step 03
+              </div>
 
-                    <p className="font-bricolage tracking-wider leading-tight text-[14px] mt-4 text-[#D3D3D3]  text-center ">Share with your friends or <br/>
-                     request them to collect all six <br/>
-                      letters of the fest!</p>
-                </div>
-                <div className="w-[20vw] h-[30vh] absolute right-[8%] bottom-0 ">
-               
-                <div className="w-[40%] h-[22%] mx-[57%] box-border border-black border-2 rounded-sm shadow-custom-white bg-yellow-400 font-bricolage text-[24.06px] font-extrabold leading-[28.87px] p-2">Step 03</div>
-
-                    <p className="font-bricolage tracking-wider leading-tight text-[14px] mt-4 text-[#D3D3D3]  text-right">Win exciting gifts <br/> and be on the <br/>winning dashboard</p>
-                    
-                </div>
-
-
+              <p className="mt-4 text-right font-bricolage text-[14px] leading-tight tracking-wider text-[#D3D3D3]">
+                Win exciting gifts <br /> and be on the <br />
+                winning dashboard
+              </p>
             </div>
           </div>
-         )}
-          </div>
-      )
-}
+        </div>
+      )}
+    </div>
+  );
+};
 
 export default HiddenQuest;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,15 +6,14 @@ export default {
   theme: {
     extend: {
       fontFamily: {
-        'neue-haas-grotesk': ['"Neue Haas Grotesk Display Pro"', 'sans-serif'],
+        "neue-haas-grotesk": ['"Neue Haas Grotesk Display Pro"', "sans-serif"],
         sans: ["var(--font-geist-sans)", ...fontFamily.sans],
-        ahsing: ['Ahsing', 'sans-serif'],
-        bricolage: ['"Bricolage Grotesque"', 'sans-serif'],
+        ahsing: ["Ahsing", "sans-serif"],
+        bricolage: ['"Bricolage Grotesque"', "sans-serif"],
       },
       boxShadow: {
-        'custom-white': '5.31px 5.31px 0px 0px #FFFFFF',
+        "custom-white": "5.31px 5.31px 0px 0px #FFFFFF",
       },
-
     },
   },
   plugins: [],
