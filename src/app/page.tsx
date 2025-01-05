3732db0a87af2f6cd3f04e028eba19bb9da8ca20
GDSC NIT Silchar <gdsc@nits.ac.in>
2025-01-05 15:21:26 +0530

    dev fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,30 @@
 import dynamic from "next/dynamic";
-
+import LandingScrollBar from "~/components/LandingScrollBar";
 const Sponsors = dynamic(() => import("~/components/Sponsors"), { ssr: false });
 
+export const runtime = "edge";
+
 const HomePage = () => {
-  return <Sponsors />;
+  return (
+    <main className="min-h-screen overflow-x-hidden bg-white">
+      <LandingScrollBar />
+      <section id="home" className="h-screen w-screen bg-[#9747ff]">
+        Home
+      </section>
+      <section id="about" className="h-screen w-screen bg-[#e23692]">
+        About Incand
+      </section>
+      <section id="about" className="h-screen w-screen bg-[#00e9f4]">
+        About NITS
+      </section>
+      <section id="sponsors" className="h-screen w-screen bg-[#b7dc68]">
+        <Sponsors />
+      </section>
+      <section id="sponsors" className="h-screen w-screen bg-[#000000]">
+        Sponsor
+      </section>
+    </main>
+  );
 };
 
 export default HomePage;
