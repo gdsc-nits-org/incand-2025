fee145a4975811f2268c54e0b9d1257dbc2d96dd
Manashgoswamii <goswamimanash02@gmail.com>
2025-01-07 12:49:07 +0530

    Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,41 @@
-<<<<<<< HEAD
+// import dynamic from "next/dynamic";
 import HiddenQuest from "~/components/HiddenQuest/HiddenQuest";
-import Button from "~/components/FooterButton/Button";
-export const runtime = "edge";
-const HomePage = () => {
-  return (
-    <main className="container mx-auto bg-white">
-      <h1 className="text-black">Home Page</h1>
-      <HiddenQuest />
-      <Button />
-=======
-import dynamic from "next/dynamic";
-import LandingScrollBar from "~/components/LandingScrollBar";
-const Sponsors = dynamic(() => import("~/components/Sponsors"), { ssr: false });
+// import LandingScrollBar from "~/components/LandingScrollBar";
+// const Sponsors = dynamic(() => import("~/components/Sponsors"), { ssr: false });
 
 // export const runtime = "edge";
 
 const HomePage = () => {
   return (
-    <main className="min-h-screen overflow-x-hidden bg-white">
-      <LandingScrollBar />
-      <section id="home" className="h-screen w-screen bg-[#9747ff]">
-        Home
-      </section>
-      <section id="about" className="h-screen w-screen bg-[#e23692]">
-        About Incand
-      </section>
-      <section id="about" className="h-screen w-screen bg-[#00e9f4]">
-        About NITS
-      </section>
-      <section id="sponsors" className="h-screen w-screen bg-[#b7dc68]">
-        <Sponsors />
-      </section>
-      <section id="sponsors" className="h-screen w-screen bg-[#000000]">
-        Sponsor
-      </section>
->>>>>>> dcb75e20b8986572714281af05e07e5c20004227
-    </main>
+    
+      <main className="container mx-auto bg-white">
+        <HiddenQuest/>
+       {/* <h1 className="text-black">Home Page</h1> */}
+       </main>
+    
+   
+   
+    // <div>
+    //    <HiddenQuest/>
+    // </div>
+    // <main className="min-h-screen overflow-x-hidden bg-white">
+    //   <LandingScrollBar />
+    //   <section id="home" className="h-screen w-screen bg-[#9747ff]">
+    //     Home
+    //   </section>
+    //   <section id="about" className="h-screen w-screen bg-[#e23692]">
+    //     About Incand
+    //   </section>
+    //   <section id="about" className="h-screen w-screen bg-[#00e9f4]">
+    //     About NITS
+    //   </section>
+    //   <section id="sponsors" className="h-screen w-screen bg-[#b7dc68]">
+    //     <Sponsors />
+    //   </section>
+    //   <section id="sponsors" className="h-screen w-screen bg-[#000000]">
+    //     Sponsor
+    //   </section>
+    // </main>
   );
 };
 
