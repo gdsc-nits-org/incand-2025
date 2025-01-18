2c58b1c49576c703e4821dc0e3e3d03209935424
byt3h3ad <adhirajd007@gmail.com>
2025-01-18 15:21:36 +0530

    fix(layout.tsx): metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,14 +14,30 @@ const OpenReplayNoSSR = dynamic(
 export const metadata: Metadata = {
   title: "Incandescence 2025",
   description: "The Official Website of Incandescence 2025",
+  metadataBase: new URL("https://incand.in"),
+  creator: "GDGC NITS",
+  keywords: [
+    "GDGC",
+    "NITS",
+    "Incandescence",
+    "2025",
+    "Incand",
+    "Incand25",
+    "NITS",
+    "NIT",
+    "Silchar",
+  ],
   icons: [{ rel: "icon", url: "/favicon.ico" }],
   robots: {
     index: true,
+    nocache: true,
+    noarchive: false,
   },
   openGraph: {
     type: "website",
     locale: "en_US",
     url: "https://incand.in",
+    images: "https://incand.in/opengraph-image.png",
   },
 };
 
