664e461e5f40ba1fc602b065dbcb49ece169143c
Priyanka Agarwal <priyanka060905@gmail.com>
2025-01-21 20:10:47 +0530

    made navbar and carpe diem (#42)
    * Made navbar desktop and CarpeDiem Page
    
    * Changes
    
    * Added responsiveness
    
    * Changes
    
    * Made changes

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,37 @@
 "use client";
 import { useEffect, useState } from "react";
 import NavbarMobile from "./NavbarMobile";
+import NavbarDesktop from "./NavbarDesktop";
+
 const Navbar = () => {
   const [isClient, setIsClient] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
 
+  // Ensures the code runs only on the client side
   useEffect(() => {
     setIsClient(true);
+    setIsMobile(window.innerWidth < 768); // Set initial state based on window size
+
+    const handleResize = () => {
+      setIsMobile(window.innerWidth < 768);
+    };
+
+    // Attach the event listener
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
 
   if (!isClient) {
     return null;
   }
+
   return (
     <>
-      <NavbarMobile />
+      {isMobile ? <NavbarMobile /> : <NavbarDesktop />}
     </>
   );
 };
