0a016d0bab116bb8e00c01b66db1540f13cd6f73
Anmol Swarup Sahoo <153299582+Newt-Shadow@users.noreply.github.com>
2025-02-03 01:28:59 +0530

    User dashboard (#58)
    * some commits related to about
    
    * test
    
    * Responsiveness in progress
    
    * Responsiveness in progress
    
    * Responsiveness Update
    
    * All Done
    
    * Added Footer , improved responsiveness
    
    * Added Footer and Improved Responsiveness
    
    * Added Footer and Improved Responsiveness
    
    * Added Footer and Improved Responsiveness
    
    * Backend integration
    
    * fixes
    
    * admin dashboard upd
    
    * fix
    
    * Updated Backend + Responsiveness
    
    * Updated Backend + Responsiveness
    
    * minor fix
    
    * loader
    
    * Loader Fix
    
    * Loader Fix
    
    * Loader Fix
    
    * Loader Fix
    
    * Loader Fix
    
    * Loader Fix
    
    * fix rm loader page
    
    * Loader Animation
    
    * arr increase
    
    ---------
    
    Co-authored-by: rishinhk004 <rishirajsaha555@gmail.com>

---
+++ src/components/Loader/loader.tsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+import {
+  useMotionValue,
+  useTransform,
+  animate,
+  useMotionValueEvent,
+} from "framer-motion";
+import { DotLottieReact } from "@lottiefiles/dotlottie-react";
+import gsap from "gsap";
+
+const LoadingScreen = () => {
+  const count = useMotionValue(0);
+  const rounded = useTransform(count, Math.round);
+  const [progress, setProgress] = useState(100);
+  const [isVisible, setIsVisible] = useState(true);
+  const [loadingFinished, setLoadingFinished] = useState(true);
+
+  useEffect(() => {
+    const controls = animate(count, 100, {
+      duration: 5,
+    });
+
+    return () => controls.stop();
+  }, [count]);
+
+  useMotionValueEvent(rounded, "change", (latest) => {
+    setProgress(latest);
+    if (latest === 100) {
+      setTimeout(() => {
+        setIsVisible(false);
+        setLoadingFinished(false);
+      }, 500);
+    }
+  });
+
+  useEffect(() => {
+    gsap.to(".counter", 0.25, {
+      delay: 5,
+      opacity: 0,
+    });
+
+    gsap.to(".bar", 1.5, {
+      delay: 4.5,
+      height: 0,
+      stagger: {
+        amount: 0.5,
+      },
+      ease: "power4.inOut",
+    });
+  }, []);
+
+  return (
+    <div className="fixed left-0 top-0 z-[1000] flex h-screen w-screen items-center justify-center">
+      <div></div>
+      <div className="counter fixed z-[10] flex h-full w-full items-center justify-center font-tusker text-[2.5rem] text-[#ffffff]">
+        <div className="z-[200] flex flex-col items-center gap-y-4 font-tusker">
+          <DotLottieReact
+            src="https://lottie.host/1ec34d07-86b0-4b73-a4f3-87048b85e8e0/afqLS8MlAd.lottie"
+            className="w-[15rem]"
+            loop
+            autoplay
+          />
+          <img
+            src="/assets/Loader/flame-hold.svg"
+            className="relative top-[-2rem] h-[5rem]"
+            alt="flame"
+          />
+          <span>{progress}%</span>
+        </div>
+      </div>
+      <div className="overlay z-2 fixed flex h-[100vh] w-[100vw]">
+        {Array(30)
+          .fill(0)
+          .map((_, i) => (
+            <div key={i} className="bar h-[105vh] w-[5vw] bg-[#1a1a1a]"></div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default LoadingScreen;
